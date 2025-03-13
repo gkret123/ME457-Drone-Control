d547bcb626441627e9fe6a5f4e9545b43d8b4a30
ASachoTanzer <adin.sachotanzer@cooper.edu>
2025-03-13 15:30:38 -0400

    added motor control, did linear stability in class stuff

--- Linear Stability/lin_stab.py
+++ Linear Stability/lin_stab.py
@@ -7,12 +7,24 @@ from mavsim_python_chap5_model_coef import *
 
 # longitudinal states: u, w, q, theta, h
 # inputs: delta_e, delta_t
-w, v = LA.eig(A_lon)
+"""w, v = LA.eig(A_lon)
 #B_lon = B_lon[:,0:0]
 print(B_lon.shape) # only elevator input: delta_e
 C_lon = np.eye(5)
 D_lon = np.zeros((5,2 ))
-sys = ss(A_lon, B_lon, C_lon, D_lon)
+sys = ss(A_lon, B_lon, C_lon, D_lon)"""
+
+
+# lateral states: v, p, r, phi, psi
+# inputs: delta_a, delta_r
+w, v = LA.eig(A_lat)
+#B_lon = B_lon[:,0:0]
+print(B_lat.shape)
+C_lat = np.eye(5)
+D_lat = np.zeros((5,2 ))
+sys = ss(A_lat, B_lat, C_lat, D_lat)
+
+print(w)
 
 fig, ax = plt.subplots()
 ax.set_xlabel(r'$Re$')
@@ -23,15 +35,16 @@ plt.show()
 
 # Step response for the system
 t = np.linspace(0,100,5000)
-y, t = impulse(sys, t)
+y, t = impulse(sys, t, input=0)
 fig, ax = plt.subplots()
 color = 'tab:blue'
 ax.set_xlabel(r'$t$ [s]')
 ax.set_ylabel(r'states')
-ax.plot(t, y[:,0,0],label='u')
-ax.plot(t, y[:,1,0],label='w')
-ax.plot(t, y[:,2,0],label='q')
-ax.plot(t, y[:,3,0],label='theta')
+ax.plot(t, y[:,0,0],label='v')
+ax.plot(t, y[:,1,0],label='p')
+ax.plot(t, y[:,2,0],label='r')
+ax.plot(t, y[:,3,0],label='$\phi$')
+ax.plot(t, y[:,4,0],label='$\psi$')
 #ax.plot(t, y[:,4,0],label='h')
 ax.legend()
 plt.show()
