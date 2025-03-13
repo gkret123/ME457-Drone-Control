22dd6b979274f2f24452b95402a9c82bd91b6e0f
ASachoTanzer <adin.sachotanzer@cooper.edu>
2025-03-13 15:57:46 -0400

    did motor pid control

--- Motor Control/dcmotor_control/example_pid.py
+++ Motor Control/dcmotor_control/example_pid.py
@@ -2,25 +2,30 @@ import numpy as np
 import parameters as P
 from integrators import get_integrator
 from pid import PIDControl
+from matplotlib import pyplot as plt
 
 
 class Controller:
     def __init__(self):
-        pass
+        self.pid = PIDControl(P.kp, P.ki, P.kd, P.umax, P.sigma, P.Ts)
         
     def update(self, r, y):
-        pass
+        return self.pid.PID(r, y)
     
 class System:
-    def __init__(self):
-        pass        
-    
-   
+    def __init__(self, x0):
+        self.integrator = get_integrator(P.Ts, self.f, integrator="RK4")
+        self.x = x0
+
+    def f(self, t, x, u):
+        return (P.K*u - x)/P.tau
+
     def update(self, u):
-        pass
+        self.x = self.integrator.step(0, self.x, u)
+        return self.x
 
 # Init system and feedback controller
-system = System()
+system = System(0)
 controller = Controller()
 
 # Simulate step response
@@ -41,6 +46,20 @@ for i in range(P.nsteps):
     u_history.append(u)
 
 # Plot response y due to step change in r
+fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
 
+# Plot response y due to step change in r
+ax1.plot(t_history, y_history)
+ax1.set_ylabel('Response')
+ax1.set_title('Response due to step change in setpoint')
+ax1.grid()
 
 # Plot actuation signal
+ax2.plot(t_history, u_history)
+ax2.set_xlabel('Time [s]')
+ax2.set_ylabel('Actuation Signal')
+ax2.set_title('Actuation Signal')
+ax2.grid()
+
+plt.tight_layout()
+plt.show()
