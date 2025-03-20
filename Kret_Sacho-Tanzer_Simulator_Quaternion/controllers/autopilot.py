5d516cff0b5be42b5704fc054c426557ab50e4eb
Gabriel Kret <gabrielkret123@gmail.com>
2025-03-20 15:57:16 -0400

    added PID cont. to autopilot

--- Kret_Sacho-Tanzer_Simulator_Quaternion/controllers/autopilot.py
+++ Kret_Sacho-Tanzer_Simulator_Quaternion/controllers/autopilot.py
@@ -72,56 +72,34 @@ class Autopilot:
         return output
     
     def update(self, cmd, state):
-	
-	#### TODO #####
-        # lateral autopilot
-        errorAirspeed = state.Va - cmd.airspeed_command
-        chi_c = wrap(cmd.course_command, state.chi)
-        errorCourse = self.saturate(state.chi - chi_c, -np.radians(15), np.radians(15))
-        self.integratorCourse = self.integratorCourse + (self.Ts/2) * (errorCourse + self.errorCourseD1)
-        self.errorCourseD1 = errorCourse
-        xLat = np.array([[errorAirspeed * np.sin(state.beta)],  # v
-                    [state.p],
-                    [state.r],
-                    [state.phi],
-                    [errorCourse],
-                    [self.integratorCourse]])
-        
-        tmp = -self.Klat @ xLat
-        delta_a = self.saturate(tmp.item(0), -np.radians(30), np.radians(30))
-        delta_r = self.saturate(tmp.item(1), -np.radians(30), np.radians(30))
 
+        # lateral autopilot
+        chi_c = wrap(cmd.course_command, state.chi)
+        phi_c = self.saturate(
+            cmd.phi_feedforward + self.course_from_roll.update(chi_c, state.chi),
+            -np.radians(30), np.radians(30)
+        )
+        delta_a = self.roll_from_aileron.update(phi_c, state.phi, state.p)
+        delta_r = self.yaw_damper.update(state.r)
 
         # longitudinal autopilot
-        altitude_c = self.saturate(cmd.altitude_command, 
-                          state.altitude - 0.2*AP.altitude_zone, 
-                          state.altitude + 0.2*AP.altitude_zone)
-    
-        errorAltitude = state.altitude - altitude_c
-        self.integratorAltitude = self.integratorAltitude \
-                                + (self.Ts/2) * (errorAltitude + self.errorAltitudeD1)
-        self.errorAltitudeD1 = errorAltitude
-        self.integratorAirspeed = self.integratorAirspeed \
-                                + (self.Ts/2) * (errorAirspeed + self.errorAirspeedD1)
-        self.errorAirspeedD1 = errorAirspeed
+        # saturate the altitude command
+        h_c = self.saturate(cmd.altitude_command,
+                            state.altitude - AP.altitude_zone, 
+                            state.altitude + AP.altitude_zone)
 
-        xLon = np.array([[errorAirspeed * np.cos(state.alpha)],  # u
-                    [errorAirspeed * np.sin(state.alpha)],  # w
-                    [state.q],
-                    [state.theta],
-                    [errorAltitude],
-                    [self.integratorAltitude],
-                    [self.integratorAirspeed]])
+        theta_c = self.altitude_from_pitch.update(h_c, state.altitude)
+        delta_e = self.pitch_from_elevator.update(theta_c, state.theta, state.q)
+        delta_t = self.airspeed_from_throttle.update(cmd.airspeed_command,
+                                                     state.Va)
 
-        tmp = -self.Klon @ xLon
-        delta_e = self.saturate(tmp.item(0), -np.radians(30), np.radians(30))
-        delta_t = self.saturate(tmp.item(1), 0.0, 1.0)
-
+        delta_t = self.saturate(delta_t, 0.0, 1.0)
+       
         # construct control outputs and commanded states
-        delta = MsgDelta(elevator=0,
-                         aileron=0,
-                         rudder=0,
-                         throttle=0)
+        delta = MsgDelta(elevator=delta_e,
+                         aileron=delta_a,
+                         rudder=delta_r,
+                         throttle=delta_t)
         self.commanded_state.altitude = 0
         self.commanded_state.Va = 0
         self.commanded_state.phi = 0
@@ -132,8 +110,11 @@ class Autopilot:
 from models.mav_dynamics_control import MavDynamics
 from message_types.msg_autopilot import MsgAutopilot
 
-AP = Autopilot(0.01)
+AutoP = Autopilot(0.01)
 MAV = MavDynamics(0.01)
 state = MsgState()
 cmd = MsgAutopilot()
-print(AP.update(cmd, state))
+delta, state = AutoP.update(cmd, state)
+delta.print()
+
+
