042dd64be2ae86007593a639e2c7ad43c57da13a
ASachoTanzer <adin.sachotanzer@cooper.edu>
2025-04-03 10:25:37 -0400

    started defining parameters in terms of zeta, wn, aerosonde parameters

--- Kret_Sacho-Tanzer_Simulator_Quaternion/parameters/control_parameters.py
+++ Kret_Sacho-Tanzer_Simulator_Quaternion/parameters/control_parameters.py
@@ -6,42 +6,61 @@ import parameters.aerosonde_parameters as MAV
 #### TODO #####
 gravity = MAV.gravity  # gravity constant
 Va0 = 36 # TF.Va_trim
+Va_trim = 36 # TF.Va_trim
+alpha_trim = 0.0 # TF.alpha_trim
+delta_trim = 1.4 # TF.delta_trim
+theta_trim = 0.0 # TF.theta_trim
 rho = 1.293 # kg/m3 # density of air (From google)
 sigma = 1  # low pass filter gain for derivative
 
+a_phi_1 = -1/2 * MAV.rho * (Va_trim ** 2) *  MAV.S_wing * MAV.b * MAV.C_p_p * MAV.b / (2 * Va_trim)
+a_phi_2 = 1/2 * MAV.rho * (Va_trim ** 2) *  MAV.S_wing * MAV.b * MAV.C_p_delta_a
+a_theta_1 = -((MAV.rho * Va_trim ** 2 * MAV.c * MAV.S_wing) / (2 * MAV.Jy)) * MAV.C_m_q
+a_theta_2 = -((MAV.rho * Va_trim ** 2 * MAV.c * MAV.S_wing) / (2 * MAV.Jy)) * MAV.C_m_alpha
+a_theta_3 = ((MAV.rho * Va_trim ** 2 * MAV.c * MAV.S_wing) / (2 * MAV.Jy)) * MAV.C_m_delta_e
+
+a_v_1 = ((MAV.rho * Va_trim * MAV.S_wing) / MAV.mass) * \
+           (MAV.C_D_0 + MAV.C_D_alpha * alpha_trim + MAV.C_D_delta_e * delta_trim) - \
+            (MAV.rho * MAV.S_prop * MAV.C_prop * Va_trim / MAV.mass)
+           #dT_dVa(delta_trim, Va_trim) / MAV.mass # AT : Not sure where I got this from (slides?) but the dt_dtva stuff is incorrect
+    #a_V2 = dT_ddelta_t(mav, delta_trim, Va_trim) / MAV.mass
+a_v_2 = (MAV.rho * MAV.S_prop * MAV.C_prop * MAV.k_motor ** 2 ) / MAV.mass
+a_v_3 = MAV.gravity * np.cos(theta_trim - alpha_trim)
+
+
 #----------roll loop-------------
 # get transfer function data for delta_a to phi
-wn_roll = 0
-zeta_roll = 0
-roll_kp = 3  # K_u = 8, T_u = 0.2 s
-roll_kd = .6
+wn_roll = 20
+zeta_roll = 0.7
+roll_kp = wn_roll**2/a_phi_2
+roll_kd = (2*zeta_roll*wn_roll-a_phi_1)/a_phi_2
 
 #----------course loop-------------
 wn_course = 1
 zeta_course = 0.7
-course_kp = 4.5  # K_u = 10, T_u = 1 s
-course_ki = 5.4
+course_kp = 2*zeta_course*wn_course*Va0/gravity  # maybe Va0 should be Vg
+course_ki = wn_course**2*Va0/gravity
 
 #----------yaw damper-------------
 yaw_damper_p_wo = 1
 yaw_damper_kr = 1
 
 #----------pitch loop-------------
-wn_pitch = 0
-zeta_pitch = 0 
-pitch_kp = 10
-pitch_kd = 0.16
-K_theta_DC = 0.5
+wn_pitch = 20
+zeta_pitch = 0.7
+pitch_kp = (wn_pitch**2-a_theta_2)/a_theta_3
+pitch_kd = (2*zeta_pitch*wn_pitch-a_theta_1)/a_theta_3
+K_theta_DC = pitch_kp*a_theta_3/(a_theta_2+pitch_kp*a_theta_3)
 
 #----------altitude loop-------------
-wn_altitude = 0
-zeta_altitude = 0
-altitude_kp = 1
-altitude_ki = 0.1
+wn_altitude = 1
+zeta_altitude = 0.7
+altitude_kp = 2*zeta_altitude*wn_altitude/(K_theta_DC*Va0)  # maybe Va0 should be Vg
+altitude_ki = wn_altitude**2/(K_theta_DC*Va0)
 altitude_zone = 5
 
 #---------airspeed hold using throttle---------------
-wn_airspeed_throttle = 0
-zeta_airspeed_throttle = 0
-airspeed_throttle_kp = 10
-airspeed_throttle_ki = 0.1
+wn_airspeed_throttle = 10
+zeta_airspeed_throttle = 0.7
+airspeed_throttle_kp = 2(*zeta_airspeed_throttle*wn_airspeed_throttle-a_v_1)/a_v_2
+airspeed_throttle_ki = wn_airspeed_throttle**2/a_v_2
---
+++ Kret_Sacho-Tanzer_Simulator_Quaternion/parameters/control_parameters_MANUAL.py
@@ -0,0 +1,47 @@
+import numpy as np
+# import design_projects.chap05.model_coef as TF
+import parameters.aerosonde_parameters as MAV
+
+
+#### TODO #####
+gravity = MAV.gravity  # gravity constant
+Va0 = 36 # TF.Va_trim
+rho = 1.293 # kg/m3 # density of air (From google)
+sigma = 1  # low pass filter gain for derivative
+
+#----------roll loop-------------
+# get transfer function data for delta_a to phi
+wn_roll = 0
+zeta_roll = 0
+roll_kp = 3  # K_u = 8, T_u = 0.2 s
+roll_kd = .6
+
+#----------course loop-------------
+wn_course = 1
+zeta_course = 0.7
+course_kp = 4.5  # K_u = 10, T_u = 1 s
+course_ki = 5.4
+
+#----------yaw damper-------------
+yaw_damper_p_wo = 1
+yaw_damper_kr = 1
+
+#----------pitch loop-------------
+wn_pitch = 0
+zeta_pitch = 0 
+pitch_kp = 10
+pitch_kd = 0.16
+K_theta_DC = 0.5
+
+#----------altitude loop-------------
+wn_altitude = 0
+zeta_altitude = 0
+altitude_kp = 1
+altitude_ki = 0.1
+altitude_zone = 5
+
+#---------airspeed hold using throttle---------------
+wn_airspeed_throttle = 0
+zeta_airspeed_throttle = 0
+airspeed_throttle_kp = 10
+airspeed_throttle_ki = 0.1
